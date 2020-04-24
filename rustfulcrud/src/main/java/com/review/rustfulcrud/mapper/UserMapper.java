5245f4f05cff83adbb41ac4c0c852712efadee5d
gitroot <314817101@qq.com>
2020-04-24 13:48:42 +0800

    0.0.1

--- rustfulcrud/src/main/java/com/review/rustfulcrud/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.review.rustfulcrud.mapper;
-
-import com.review.rustfulcrud.bean.User;
-import org.apache.ibatis.annotations.Select;
-
-public interface UserMapper {
-    @Select("SELECT*FROM`user`WHERE`userName`=#{userName}")
-    public User getUser(String userName);
-}
